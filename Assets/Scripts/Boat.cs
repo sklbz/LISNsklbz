fbf0ab9f65e4f65ab2a050cb1bc445555f10613a
sklbz <sokolbozanic@gmail.com>
2023-06-19 15:41:41 +0200

    commit

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -2,4 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boat : MonoBehaviour { }
+public class Boat : MonoBehaviour {
+    public int keyNumber;
+    string key;
+    string[] keys = {"q", "a", "r"};
+    Vector3 speed = new Vector3 (0, 0.1f, 0);
+    void Start() {
+        key = keys[keyNumber];
+        Debug.Log(key);
+        Debug.Log(keyNumber);
+    }
+    void Update() {
+        if (Input.GetKeyDown(keys[keyNumber]))
+        {
+            transform.position = transform.position + speed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "FinishLine")
+        {
+            Debug.Log("Team {key} won!");
+        }
+    }
+}
