aacfdb01a908611ae1a74967503066e8d2e8c77f
sklbz <sokolbozanic@gmail.com>
2023-06-19 14:27:20 +0200

    commit

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BoatController : MonoBehaviour
 {
+    public GameObject[] boat;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("a"))
+        {
+            boat[0].transform.position = new Vector3();
+        }
     }
 }
