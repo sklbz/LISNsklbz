cc69c251bc68c5ec4dac43669046a25623b7c1f2
sklbz <sokolbozanic@gmail.com>
2023-06-23 11:18:46 +0200

    commit

--- Assets/Scripts/Accelerometer.cs
+++ Assets/Scripts/Accelerometer.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Accelerometer : MonoBehaviour
 {
+    [SerializeField]
+    float speed = 10.0f;
     private Rigidbody _rb;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
-    {
-        _rb.AddForce(Input.acceleration * Time.deltaTime);
+    void Update() {
+        Vector3 _velocity = Vector3.zero;
+
+        _velocity.x = -Input.acceleration.y;
+        _velocity.z = Input.acceleration.x;
+
+        if (_velocity.sqrMagnitude > 1)
+        {
+            _velocity.Normalize();
+        }
+
+        _rb.AddForce(speed * Time.deltaTime * _velocity);
     }
 }
